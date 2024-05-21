507976bd1248552a5626abba17ed801fdaeeaf74
dianfitrip <dianfitri683@gmail.com>
2024-05-21 11:10:21 +0700

    memanggil header file di main file(composition)

--- compotition(Relasi)/compotition(Relasi).cpp
+++ compotition(Relasi)/compotition(Relasi).cpp
@@ -2,4 +2,12 @@
 #include <string>
 using namespace std;
 
+#include "jantung.h"
+#include "manusia.h"
+
+int main() {
+	Manusia* varManusia = new Manusia("jono");
+	delete varManusia;
+	return 0;
+}
 
