be8eef85f4a9319a9998c291943afaea2e8f1075
dianfitrip <dianfitri683@gmail.com>
2024-05-21 11:43:47 +0700

    int main memanggil header file anak dan ibu

--- Agregation(Relasi)/Agregation(Relasi).cpp
+++ Agregation(Relasi)/Agregation(Relasi).cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+#include "anak.h"
+#include "ibu.h"
+
+int main() {
+	Ibu* varIbu = new Ibu("dini");
+	Ibu* varIbu2 = new Ibu("novi");
+	Anak* varAnak1 = new Anak("tono");
+	Anak* varAnak2 = new Anak("rini");
+	Anak* varAnak3 = new Anak("Dewi");
+
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
+}
--- Agregation(Relasi)/ibu.h
+++ Agregation(Relasi)/ibu.h
@@ -1,28 +1,29 @@
 #ifndef IBU_H
 #define IBU_H
+
 class Ibu {
 public:
 	string nama;
-	vector<anak*> daftar_anak;
+	vector<Anak*> daftar_Anak;
 
-	ibu(string pNama) :nama(pNama) {
+	Ibu(string pNama) :nama(pNama) {
 		cout << "Ibu \"" << nama << "\" ada\n";
 	}
 	~Ibu() {
-		cout << "Ibu \"" << nama "\"tidak ada\n";
+		cout << "Ibu \"" << nama "\" tidak ada\n";
 	}
 
-	void tambahAnak(anak*);
+	void tambahAnak(Anak*);
 	void cetakAnak();
 };
 
-void Ibu::tambahAnak(anak* pAnak) {
+void Ibu::tambahAnak(Anak* pAnak) {
 	daftar_anak.push_back(pAnak);
 }
 
 void Ibu::cetakAnak() {
 	cout << "Daftar Anak dari Ibu \"" << this->nama << "\:\n";
-	for (aotu& a : daftar_anak) {
+	for (auto& a : daftar_anak) {
 		cout << a->nama << "\n";
 	}
 	cout << endl;
